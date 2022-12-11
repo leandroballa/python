6106094f276ed9b2ac5adee4c1c98f8863090527
Leandro Balla <leandro.balla@outlook.com>
2022-12-11 19:32:17 +1030

    Create ex003.py

---
+++ learning/mod01/ex003.py
@@ -0,0 +1,7 @@
+n1 = input('Digite um número ')
+n2 = input('Digite um número ')
+result = int(n1)+int(n2)
+# print('O resultado é', result)
+# print('O resultado é {}'.format(result))
+# print('A soma entre', n1, ' e', n2, 'vale', result)
+print('A soma entre {} e {} vale {}!'.format(n1, n2, result))
