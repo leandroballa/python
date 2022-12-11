759262781ae1d93ca7469d61d44b2557b806373f
Leandro Balla <leandro.balla@outlook.com>
2022-12-11 19:36:53 +1030

    Create ex072.py

---
+++ learning/mod03/ex072.py
@@ -0,0 +1,8 @@
+extenso = ('ZERO', 'UM', 'DOIS', 'TRÊS', 'QUATRO', 'CINCO', 'SEIS', 'SETE', 'OITO', 'NOVE', 'DEZ'
+           , 'ONZE', 'DOZE', 'TREZE', 'QUATORZE', 'QUINZE', 'DEZESSEIS', 'DEZESSETE', 'DEZOITO', 'DEZENOVE', 'VINTE')
+num = -1
+
+while num not in (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20):
+    num = int(input('Digite um número entre 0 e 20: '))
+
+print(f'Você digitou o número {extenso[num]}')
